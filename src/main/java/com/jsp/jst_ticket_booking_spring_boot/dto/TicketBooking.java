a79c377d6c9e7968a343a1051ce4a106a85fab27
Akshit <akshitaggarwal502@gmail.com>
2024-10-17 12:56:32 +0530

    commit

---
+++ src/main/java/com/jsp/jst_ticket_booking_spring_boot/dto/TicketBooking.java
@@ -0,0 +1,29 @@
+package com.jsp.jst_ticket_booking_spring_boot.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TicketBooking {
+    @Id
+    private long pnr;
+    private String passengerName;
+    private int passengerAge;
+    private String gender;
+    private LocalDate journeyDate;
+
+    @ManyToOne
+    private Customer customer;
+
+    @OneToOne
+    private Train train;
+}
